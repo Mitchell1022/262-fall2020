ab0c05f638527f9987581b65632386a143d45177
CanadianMitch16 <mitchellniesar@gmail.com>
2020-10-04 00:35:36 -0400

    Videos Implemented

--- lab05/gamezone/shared/header.js
+++ lab05/gamezone/shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ navigation }) {
@@ -10,7 +10,26 @@ export default function Header({ navigation }) {
 
     return (
         <View>
-            <MaterialIcons name='menu' size={28} onPress={openMenu} />
+            <MaterialIcons name='menu' size={28} onPress={openMenu} style={StyleSheet.icon} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#333',
+        letterSpacing: 1,
+    },
+    icon: {
+        left: 16,
+    }
+})
